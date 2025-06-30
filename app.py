c5dade878697efd14bc61e54522acd0a8a9bbdba
unknown <macaulim.official@gmail.com>
2025-06-30 15:32:09 -0300

    commitando

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import json
 
 url = 'https://api.the-odds-api.com/v4/sports?apiKey=92a0209828337b02e378101efc68d6f9' 
 response = requests.get(url)
-print (response)
+dados = response.json()
+print(dados)
