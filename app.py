0a57fbad39a6ecb1effc5cdf53f3daeb4e637ad5
unknown <macaulim.official@gmail.com>
2025-07-02 13:44:53 -0300

    arrumando o .env

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import requests
 import json
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
-url = 'https://api.the-odds-api.com/v4/sports?apiKey=92a0209828337b02e378101efc68d6f9' 
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+url = f'https://api.the-odds-api.com/v4/sports?apiKey={api_key}'
+
+
 response = requests.get(url)
-dados = response.json()
-print(dados)
+
+if response.status_code == 200:
+    dados = response.json()
+    df = pd.DataFrame(dados)
+    print(df.head())
+else:
+    print("Erro ao acessar API:", response.status_code)
